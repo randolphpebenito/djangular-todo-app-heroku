da0e2bd3875475453d2df5a84839e14fac6eb7be
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 20:56:12 +0800

    Added admin view for Todo model.

--- todo/admin.py
+++ todo/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'note', 'completed', 'created_at', 'updated_at')
+
+
+admin.site.register(Todo, TodoAdmin)
+admin.site.site_header = 'Todo API'
