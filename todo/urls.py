06784a5e86f94ea3da50fb30636742fa210e6444
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 17:40:20 +0800

    Added url for TodoRetrieveUpdateDelete View.

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import TodoListCreateView
+from .views import TodoListCreateView, TodoRetrieveUpdateDeleteView
 
 app_name = 'todo'
 urlpatterns = [
     url(r'^$', TodoListCreateView.as_view(), name='todo-list-create'),
+    url(r'^(?P<pk>[0-9]+)$', TodoRetrieveUpdateDeleteView.as_view(), name='todo-retrieve-update-delete'),
 ]
 
 
