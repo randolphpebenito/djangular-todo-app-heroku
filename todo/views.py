8fa120063a74b36fad168ec9ef6df4d1ee4a3770
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 14:50:21 +0800

    Created Todo List Create View with sorted by date desc func.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoListCreateView(ListCreateAPIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        """
+            Sort Todo List by its latest date (updated)
+        """
+        return Todo.objects.order_by('-updated_at')
