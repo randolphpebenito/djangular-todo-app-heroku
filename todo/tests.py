4a9db734f7b3997ddddf4e8b48ebd2ebe1d3844e
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 13:35:23 +0800

    Created TodoSerializer test cases.

--- todo/tests.py
+++ todo/tests.py
@@ -10,3 +10,35 @@ class TodoSerializerTest(APITestCase):
     def setUp(self):
         pass
 
+    def test_blank_fields(self):
+        data = {}
+        serializer = TodoSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_missing_title_field(self):
+        data = { "note": "This is just a note" }
+        serializer = TodoSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertRegexpMatches( str(serializer.errors), "title")
+        self.assertRegexpMatches( str(serializer.errors), "This field is required")
+
+    def test_title_char_exceeded(self):
+        data = { 
+            "title": "this title exceeds 64 char Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", 
+            "note": "This is just a note" 
+        }
+        serializer = TodoSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertRegexpMatches( str(serializer.errors), "title")
+        self.assertRegexpMatches( str(serializer.errors), "Ensure this field has no more than 64 characters.")
+
+    def test_create_todo(self):
+        data = { 
+            "title": "This is my todo", 
+            "note": "This is just a note" 
+        }
+        serializer = TodoSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        s = serializer.save()
+        self.assertEquals(s.title, "This is my todo")
+
