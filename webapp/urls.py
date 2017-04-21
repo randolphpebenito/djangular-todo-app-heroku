1854ee02f01659fea3eec50b4976b5729fee177f
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 21:46:59 +0800

    Created url for index view.

--- webapp/urls.py
+++ webapp/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+
+from todo.views import index
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^todo/', include('todo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
