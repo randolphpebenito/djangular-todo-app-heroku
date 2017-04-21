a9bb90cfbeaa9b28814ab619bcdec59394f57bc9
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-21 21:46:44 +0800

    Added index view for todo front end.

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.shortcuts import render
+
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Todo
 from .serializers import TodoSerializer
+
+def index(request):
+    """
+        Main Page (Single Page Application)
+    """
+    return render(request, "index.html", {})
 
 class TodoListCreateView(ListCreateAPIView):
     serializer_class = TodoSerializer
