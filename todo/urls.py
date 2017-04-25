82e8b4cbfb58cf83106a089d28ada521da6b9df1
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-26 01:48:35 +0800

    Reverted with trailing slash.

--- todo/urls.py
+++ todo/urls.py
@@ -5,7 +5,7 @@ from .views import TodoListCreateView, TodoRetrieveUpdateDeleteView
 app_name = 'todo'
 urlpatterns = [
     url(r'^$', TodoListCreateView.as_view(), name='todo-list-create'),
-    url(r'^/(?P<pk>[0-9]+)$', TodoRetrieveUpdateDeleteView.as_view(), name='todo-retrieve-update-delete'),
+    url(r'^(?P<pk>[0-9]+)$', TodoRetrieveUpdateDeleteView.as_view(), name='todo-retrieve-update-delete'),
 ]
 
 
